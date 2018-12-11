7122f75ed81217531a88dab61b1e18c3f3895770
David Canon <david.canon@resourceit.com>
2018-12-11 07:51:59 -0200

    Adicionado utils

---
+++ app/src/main/java/br/com/bancopan/topgames/utils/Constants.kt
@@ -0,0 +1,14 @@
+package br.com.bancopan.topgames.utils
+
+object Constants {
+    //Endpoint constants
+    val CONTENT_TYPE = "Content-Type"
+    val CONTENT_TYPE_JSON = "application/json"
+    val NETWORK_TIMEOUT: Long = 20000
+    val WRITE_TIMEOUT: Long = 30000
+    val API_LIMIT: Int = 10
+
+    //Bundle
+    val BUNDLE = "BUNDLE"
+
+}
