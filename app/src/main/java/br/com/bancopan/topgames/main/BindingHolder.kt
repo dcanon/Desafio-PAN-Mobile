9ea3b3c498e68932eb0b19a6c85a5f441fdd5ecf
David Canon <david.canon@resourceit.com>
2018-12-11 07:45:07 -0200

    Adicionado Adapter para listagem dos jogos

---
+++ app/src/main/java/br/com/bancopan/topgames/main/BindingHolder.kt
@@ -0,0 +1,7 @@
+package br.com.bancopan.topgames.main
+
+import android.support.v7.widget.RecyclerView
+
+import br.com.bancopan.topgames.databinding.ItemGameBinding
+
+class BindingHolder(var binding: ItemGameBinding) : RecyclerView.ViewHolder(binding.root)
