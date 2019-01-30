00d7094cc14e10154f2cd073d20786573f3c846f
David Canon <David.Canon@grupopan.com>
2019-01-30 17:30:49 -0200

    Realizado refac no repositório de games

--- app/src/main/java/br/com/bancopan/topgames/repository/GameRepository.kt
+++ app/src/main/java/br/com/bancopan/topgames/repository/GameRepository.kt
@@ -1,54 +1,25 @@
 package br.com.bancopan.topgames.repository
 
-import android.annotation.SuppressLint
 import br.com.bancopan.topgames.repository.api.Api
 import br.com.bancopan.topgames.repository.data.Game
 import br.com.bancopan.topgames.repository.data.GameResponse
-import br.com.bancopan.topgames.repository.data.Top
+import br.com.bancopan.topgames.repository.data.ServiceErrorModel
 import br.com.bancopan.topgames.repository.db.GameDao
 import br.com.bancopan.topgames.utils.Constants
 import br.com.bancopan.topgames.utils.RepoUtils
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import io.realm.Realm
-import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.android.Main
 import kotlinx.coroutines.launch
 import retrofit2.HttpException
-import retrofit2.Response
 import timber.log.Timber
-import java.net.ConnectException
 
 class GameRepository(val gameDao: GameDao, val api: Api) {
     var totalItems: Int = 0
-//    var listener: RepositoryListener? = null
 
-    @Deprecated("Ira ser desabilitado")
-    fun getTopGames(offset: Int) {
-//        serviceCall(offset)
-    }
-    /*
     @Synchronized
-    fun <T> serviceCall(service: Observable<T>, onResponse: (T) -> Unit, onFailure: (ServiceErrorModel) -> Unit) {
-
-        if (App.getInstance().hasInternet) {
-            service.subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(
-                            { response -> applyOnModelResponse(response, service, onResponse, onFailure) },
-                            { throwable -> applyOnThrowableResponse(throwable, service, onResponse, onFailure) }
-                    ).addTo(disposables)
-
-        } else {
-            applyOnThrowableResponse(ConnectException("No Connection available"), service, onResponse, onFailure)
-        }
-    }*/
-
-
-    fun serviceCall(offset: Int, onResponse: (List<Game>) -> Unit, onFailure: (String) -> Unit,
+    fun getTopGames(offset: Int, onResponse: (List<Game>) -> Unit, onFailure: (ServiceErrorModel) -> Unit,
                     limit: Int = Constants.API_LIMIT) {
 
         GlobalScope.launch(Dispatchers.Main) {
@@ -56,30 +27,53 @@ class GameRepository(val gameDao: GameDao, val api: Api) {
             try {
                 val request = api.getTopGames(limit, offset)
                 val response = request.await()
-
-                response.top?.apply {
-                    val games = RepoUtils.buildGameList(response.top!!)
-                    totalItems = response.total!!
-                    onResponse.invoke(games)
-                }
-
+                applyOnModelResponse(onResponse, response)
             } catch (e: Exception) {
-                onFailure.invoke(e.localizedMessage)
+                getDataFromDb(e, onResponse, onFailure)
             }
         }
 
     }
 
-    @SuppressLint("CheckResult")
-    private fun getDataFromDb() {
-        gameDao.findAll()
-                .doOnError {
-//                    listener?.onDataFailure()
+    @Synchronized
+    private fun applyOnModelResponse(onResponse: (List<Game>) -> Unit, response: GameResponse) {
+        val games = RepoUtils.buildGameList(response.top!!)
+        totalItems = response.total!!
+        onResponse.invoke(games)
+        storeGamesInDb(games)
+    }
+
+    @Synchronized
+    private fun applyOnThrowableResponse(t: Throwable, onError: (ServiceErrorModel) -> Unit) {
+        val error = ServiceErrorModel()
+        when (t) {
+            is HttpException -> {
+                error.apply {
+                    throwable = t
+                    httpCode = t.code()
+                    isConnectException = false
+                    errorMessage = t.localizedMessage
                 }
-                .subscribe {
-                    if (it.isValid && it.isNotEmpty()) {
-                        Timber.d("Loaded ${it.size} games from Db")
-//                        listener?.onDataSuccess(Realm.getDefaultInstance().copyFromRealm(it))
+            }
+            else -> {
+                error.apply {
+                    isConnectException = true
+                    errorMessage = "unknown error"
+                }
+            }
+        }
+        onError.invoke(error)
+    }
+
+    private fun getDataFromDb(t: Throwable, onResponse: (List<Game>) -> Unit, onFailure: (ServiceErrorModel) -> Unit) {
+        gameDao.findAll()
+                .doOnError { applyOnThrowableResponse(t, onFailure) }
+                .subscribe { result ->
+                    if (result.isValid && result.isNotEmpty()) {
+                        onResponse.invoke(Realm.getDefaultInstance().copyFromRealm(result))
+                        Timber.d("Loaded ${result.size} games from Db")
+                    } else {
+                        applyOnThrowableResponse(t, onFailure)
                     }
                 }
     }
