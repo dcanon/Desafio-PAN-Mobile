157568b7df55b53e86cb92d530f4300eb838a99a
David Canon <David.Canon@grupopan.com>
2019-02-01 10:59:42 -0200

    refac

--- app/src/main/java/br/com/bancopan/topgames/main/listener/GameAdapterListener.kt
+++
@@ -1,9 +0,0 @@
-package br.com.bancopan.topgames.main.listener
-
-import android.view.View
-import br.com.bancopan.topgames.repository.data.Game
-
-interface GameAdapterListener {
-
-    fun onItemClicked(view: View, Position: Int, game: Game)
-}
